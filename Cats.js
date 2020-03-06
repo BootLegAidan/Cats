f258c737cde60bd8c84f916c44996bdbd5132450
xDERPYxCREEPERx <30330512+xDERPYxCREEPERx@users.noreply.github.com>
2020-03-06 14:59:45 -0600

    Create Cats.js

---
+++ Cats.js
@@ -0,0 +1,53 @@
+var catsPics = {
+	0: {
+		name: "American Shorthair",
+		url: "https://i.ibb.co/ykGS2qH/American-Shorthair.png"
+	},
+	1: {
+		name: "British Shorthair",
+		url: "https://i.ibb.co/Tct9GVp/British-Shorthair.png"
+	},
+	2: {
+		name: "Maine Coon",
+		url: "https://i.ibb.co/MkCNBVk/Maine-Coon.png"
+	},
+	3: {
+		name: "Selkirk Rex",
+		url: "https://i.ibb.co/0JwqkBB/Selkirk-Rex-removebg-preview.png"
+	},
+	4: {
+		name: "Ragdoll",
+		url: "https://i.ibb.co/4SvgTfC/Khao-Manee.png"
+	},
+	5: {
+		name: "Toyger",
+		url: "https://i.ibb.co/nLfYG1C/Toyger.png"
+	}
+};
+var catPic;
+var catX;
+var catY;
+var windowW = window.innerWidth;
+var windowH = window.innerHeight;
+function addCat() {
+	catPic = Math.floor(Math.random() * Object.keys(catsPics).length);
+	catX = Math.random() * windowW;
+	catY = Math.random() * windowH;
+	document.body.innerHTML += `<img src="${catsPics[catPic].url}" style="bottom: ${catY}px;right:${catX}px" alt="${catsPics[catPic].name}">`;
+	//	requestAnimationFrame(addCat)
+	cats = document.getElementsByTagName("img");
+	if (cats.length > 5) {
+		if (cats[0].alt.charAt(0) == "A") {
+			console.log("An " + cats[0].alt + " was removed");
+		} else {
+			console.log("A " + cats[0].alt + " was removed");
+		}
+		cats[0].remove();
+	}
+}
+addCat();
+setInterval(addCat, 100);
+window.addEventListener("resize", function() {
+	windowW = window.innerWidth;
+	windowH = window.innerHeight;
+});
